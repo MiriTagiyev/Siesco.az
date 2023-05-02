c55f264f90db0c34526732459da168644b9a7e85
#MT <120247949+MiriTagiyev@users.noreply.github.com>
2023-05-02 17:07:36 +0400

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,412 @@
+//Merkez slide start//
+const swiper = new Swiper(".mySwiper", {
+  loop: true,
+  autoplay: {
+    delay: 3500,
+  },
+  speed: 1000,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+//Merkez slide end//
+//Mehsullar slide start//
+// function selecionarOpcao(elmnt, className, color) {
+//   var x = document.getElementsByClassName(className)[0].children;
+//   for (i = 1; i < x.length; i++) {
+//     x[i].style.backgroundColor = "#fff";
+//     x[i].style.color = "#111";
+//     x[i].style.border = "solid 1px " + color;
+//   }
+//   elmnt.style.backgroundColor = color;
+//   elmnt.style.color = "#fff";
+//   elmnt.style.border = "1px solid #eee";
+// }
+//Mehsullar slide end//
+//Xidmetler slide start//
+let index = 0;
+let interv = setInterval(() => {
+  services();
+}, 2000);
+services();
+function services() {
+  if (index == $(".service-item").length - 1) {
+    $(`.service-item:eq(${index})`).css({
+      transform: "scale(0.8)",
+      opacity: "0.5",
+    });
+    $(".service-slide-scroll").animate({ top: "-30px" });
+    index = 0;
+  }
+  $(".service-slide-scroll").animate({ top: "-" + index * 180 - 30 + "px" });
+  $(`.service-item:eq(${index})`).css({
+    transform: "scale(0.8)",
+    opacity: "0.5",
+    "box-shadow": "none",
+  });
+  $(`.service-item:eq(${index + 1})`).css({
+    transform: "scale(1)",
+    opacity: "1",
+    "box-shadow": "0 5px 15px silver",
+  });
+  index++;
+  $(".service-text").html(
+    $(`.service-item:eq(${index + 1}) .service-item-body`).html()
+  );
+}
+$(".service-info").hover(
+  () => {
+    clearInterval(interv);
+  },
+  () => {
+    interv = setInterval(() => {
+      services();
+    }, 2000);
+  }
+);
+//Xidmetler slide end//
+//Musteri slide start//
+const swipers = new Swiper(".swiper-partner", {
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false, //ユーザーがスワイプなどの操作しても止まらない
+    reverseDirection: true, // 逆方向有効化
+  },
+  loop: true,
+  spaceBetween: 10,
+  // loopedSlides: 14, //設定すると途中で止まる
+  slidesPerView: 7,
+  speed: 3000,
+  allowTouchMove: false, // スワイプ無効,
+});
+
+const swiper2 = new Swiper(".swiper2", {
+  autoplay: {
+    delay: 0, // 途切れなくループ
+    disableOnInteraction: false, //ユーザーがスワイプなどの操作しても止まらない
+  },
+  loop: true, // ループ有効
+  spaceBetween: 10,
+  // loopedSlides: 14, //設定すると途中で止まる
+  slidesPerView: 7, // 一度に表示する枚数
+  speed: 3000, // ループの時間
+  allowTouchMove: false, // スワイプ無効
+});
+//Musteri slide end//
+
+//Xidmetler slide start//
+let VerticalCarousel = function (element) {
+  this.slideUp = function () {
+    let sliders = document.querySelectorAll(element);
+    if (sliders && sliders !== null && sliders.length > 0) {
+      sliders.forEach((el) => {
+        el.lastElementChild.classList.add("vs-previous");
+        let initCloneReverse = el
+          .getElementsByClassName("vs-previous")[0]
+          .cloneNode(true);
+        el.prepend(initCloneReverse);
+        el.lastElementChild.remove();
+        el.firstElementChild.nextElementSibling.classList.add("vs-active");
+        el.firstElementChild.nextElementSibling.nextElementSibling.classList.add(
+          "vs-next"
+        );
+        setInterval(function () {
+          let elClone = el
+            .getElementsByClassName("vs-previous")[0]
+            .cloneNode(true);
+          el.appendChild(elClone);
+          elClone.classList.remove("vs-previous");
+          el.firstElementChild.remove();
+          let first = el.querySelector(".vs-active");
+          first.classList.add("vs-previous");
+          first.classList.remove("vs-active");
+          let second = el.querySelector(".vs-next");
+          document.querySelector(".service-text h2").innerText =
+            second.querySelector(".vs-carousel__item-title").innerText;
+          document.querySelector(".service-text p").innerText =
+            second.querySelector(".vs-carousel__item-description").innerText;
+          second.classList.add("vs-active");
+          second.classList.remove("vs-next");
+          second.nextElementSibling.classList.toggle("vs-next");
+        }, 3000);
+      });
+    }
+  };
+};
+let c = new VerticalCarousel(".vs-carousel");
+c.slideUp();
+//Xidmetler slide end//
+//mousestart
+document.body.onload = function () {
+  "use strict";
+  var el = document.querySelector(".rs-cf3d");
+  var cf3d = new CarouselFlow(el, {
+    mode: "flip",
+    useAutoplay: true,
+    useMousewheel: false,
+    useReflection: false,
+  });
+  window.cf3d = cf3d;
+};
+
+var xMousePos = 0;
+var yMousePos = 0;
+var lastScrolledLeft = 0;
+var lastScrolledTop = 0;
+
+$(document).mousemove(function (event) {
+  captureMousePosition(event);
+});
+
+$(window).scroll(function (event) {
+  if (lastScrolledLeft != $(document).scrollLeft()) {
+    xMousePos -= lastScrolledLeft;
+    lastScrolledLeft = $(document).scrollLeft();
+    xMousePos += lastScrolledLeft;
+  }
+  if (lastScrolledTop != $(document).scrollTop()) {
+    yMousePos -= lastScrolledTop;
+    lastScrolledTop = $(document).scrollTop();
+    yMousePos += lastScrolledTop;
+  }
+  $(".cursor").css({ top: yMousePos - 70 + "px", left: xMousePos - 70 + "px" });
+});
+function captureMousePosition(e) {
+  xMousePos = e.pageX;
+  yMousePos = e.pageY;
+  $(".cursor").css({ top: e.pageY - 70 + "px", left: e.pageX - 70 + "px" });
+}
+
+$("body").click(function () {
+  $(".cursor").addClass("expand");
+  setTimeout(() => {
+    $(".cursor").removeClass("expand");
+  }, 500);
+});
+
+//mouseend
+//oclock start
+
+$.fn.jQuerySimpleCounter = function (options) {
+  var settings = $.extend(
+    {
+      start: 0,
+      end: 100,
+      easing: "swing",
+      duration: 400,
+      complete: "",
+    },
+    options
+  );
+
+  var thisElement = $(this);
+
+  $({ count: settings.start }).animate(
+    { count: settings.end },
+    {
+      duration: settings.duration,
+      easing: settings.easing,
+      step: function () {
+        var mathCount = Math.ceil(this.count);
+        thisElement.text(mathCount);
+      },
+      complete: settings.complete,
+    }
+  );
+};
+
+$("#number1").jQuerySimpleCounter({ end: 12, duration: 3000 });
+$("#number2").jQuerySimpleCounter({ end: 55, duration: 3000 });
+$("#number3").jQuerySimpleCounter({ end: 359, duration: 2000 });
+$("#number4").jQuerySimpleCounter({ end: 246, duration: 2500 });
+
+/* AUTHOR LINK */
+$(".about-me-img").hover(
+  function () {
+    $(".authorWindowWrapper").stop().fadeIn("fast").find("p").addClass("trans");
+  },
+  function () { 
+    $(".authorWindowWrapper")
+      .stop()
+      .fadeOut("fast")
+      .find("p")
+      .removeClass("trans");
+  }
+);
+
+//oclock end
+
+
+(function (e) {
+  e.fn.circle = function (t) {
+    var n = e.extend({
+      speed: "5000"
+    }, t);
+    return this.each(function () {
+      function t() {
+        var e = i.find("li.block.active").index();
+        c.removeClass("active"), c.eq(e).addClass("active")
+      }
+
+      function o() {
+        var n;
+        i.addClass("disable-hover"), i.stop(!0, !0).animate({
+          rotatedeg: p.deg += p.step
+        }, {
+          duration: 400,
+          step: function (t) {
+            t >= 360 ? t -= 360 : t <= -360 && (t += 360), e(this).css("transform", "rotate(" + t + "deg)"), e(this).css("-webkit-transform", "rotate(" + t + "deg)")
+          },
+          complete: function () {
+            p.active = i.find("li.active").removeClass("active"), "right" == p.direction && p.step == d && (p.active.prev() && p.active.prev().length ? (n = p.active.prev().index(), p.active.prev().addClass("active")) : (p.active.siblings(":last-child").addClass("active"), n = 9)), "left" == p.direction && p.step == u && (p.active.next() && p.active.next().length ? (n = p.active.next().index(), p.active.next().addClass("active")) : (p.active.siblings(":first-child").addClass("active"), n = 0)), i.is(":animated"), i.removeClass("disable-hover"), t()
+            $('header .lotties .current').removeClass('current')
+            $(`header .lotties .lottie:eq(${n})`).addClass('current')
+            $('header .detail-texts .current').removeClass('current')
+            $(`header .detail-texts .detail-text:eq(${n})`).addClass('current')
+          }
+        }, "ease")
+      }
+
+      function r() {
+        i.addClass("disable-hover"),
+          i.stop(!0, !0).animate(
+            {
+              rotatedeg: p.deg += p.step
+            },
+            {
+              duration: 400,
+              step: function (t) {
+                t >= 360 ? t -= 360 : t <= -360 && (t += 360),
+                  e(this).css("transform", "rotate(" + t + "deg)"),
+                  e(this).css("-webkit-transform", "rotate(" + t + "deg)")
+              },
+              complete: function () {
+                p.active = i.find("li.active"),
+                  i.is(":animated"),
+                  i.removeClass("disable-hover")
+              }
+            }, "ease")
+      }
+      var i = e(this),
+        s = i.find("li").length,
+        a = i.find(" > li .icon"),
+        l = "count" + s,
+        u = 0,
+        c = i.next().find(".animate"),
+        p = {
+          duration: n.speed,
+          deg: 0,
+          step: u,
+          active: i.find("li.active"),
+          direction: "left"
+        };
+      switch (s) {
+        case 10:
+          u = -36;
+          break;
+        case 9:
+          u = -40;
+          break;
+        case 8:
+          u = -45;
+          break;
+        case 7:
+          u = -51.5;
+          break;
+        case 6:
+          u = -60;
+          break;
+        case 5:
+          u = -72;
+          break;
+        case 4:
+          u = -90;
+          break;
+        case 3:
+          u = -120;
+          break;
+        case 2:
+          u = -180;
+          break;
+        case 1:
+          u = -360
+      }
+      i.addClass(l);
+      var d = u - 2 * u;
+      i.find("> li").first().addClass("active"),
+        i.find("> li").first().find("a").attr("href"),
+        c.eq(0).addClass("active"),
+        e(a).on("click", function () {
+          $('header .lotties .current').removeClass('current')
+          $(`header .lotties .lottie:eq(${e(this).parent().index()})`).addClass('current')
+          $('header .detail-texts .current').removeClass('current')
+          $(`header .detail-texts .detail-text:eq(${e(this).parent().index()})`).addClass('current')
+          var n = e(this).parent().index() - i.find("li.active").index();
+          i.children("li").removeClass("active"),
+            e(this).parent().addClass("active"),
+            p.step = -n * d, n * d >= 288 && (p.step = -n * d + 360),
+            n * d <= -288 && (p.step = -n * d - 360),
+            r(), p.step = u, p.direction = "left", t()
+        }); 
+      var f = i.parent().find("div.next"),
+        h = i.parent().find("div.prev");
+      f.on("click", function () {
+        //sag click
+        i.is(":animated") || (p.direction = "left", p.step = u, o())
+
+      }), h.on("click", function () {
+        //sol click
+        i.is(":animated") || (p.direction = "right", p.step = d, o())
+      })
+    })
+  }
+  setInterval(() => {
+    $('.next').click()
+  }, 8000)
+
+  $('header>.prev').click(()=>{
+    $('header .services .prev').click()
+  })
+  $('header>.next').click(()=>{
+    $('header .services .next').click()
+  })
+}(jQuery));
+
+
+$(function () {
+  //Background image
+  $('div').each(function () {
+    var url = $(this).attr('data-image');
+    if (url) {
+      $(this).css('background-image', 'url(' + url + ')');
+    }
+  });
+
+  $('section').each(function () {
+    var url = $(this).attr('data-image');
+    if (url) {
+      $(this).css('background-image', 'url(' + url + ')');
+    }
+  });
+
+  // Service Auto height circle 
+  function autoHeightCircle() {
+    var circle = $('.circle--rotate'),
+      circleInner = $('.animate-wrapper'),
+      circleH = circle.width(),
+      circleInnerH = circleInner.width();
+    circle.height(circleH);
+    circleInner.height(circleInnerH);
+  }
+
+  $("#circle--rotate").circle();
+  autoHeightCircle();
+  $(window).resize(function () {
+    autoHeightCircle();
+  });
+});
---
+++ product.js
@@ -0,0 +1,67 @@
+var menu = [
+  {
+    basliq: "1C: Mühasibatlıq 3.0",
+    info1:
+      "'1C: Mühasibat uçotu 8' - mühasibat uçotunun avtomatlaşdırılmasını tamamilə yeni səviyyəyə qaldıra bilən ən populyar mühasibat proqramıdır. Rahat məhsul və ona qoşulan xidmətlər istənilən biznesin mühasibat xidmətinin problemlərini effektiv həll etməyə imkan verəcək.1C istifadəçilərin ehtiyac və tapşırıqlarına cavab verən müasir və çox yönlü mühasibat həlli təklif etmək üçün proqramı və xidmətləri daim təkmilləşdirir.'1C: Mühasibat 8 nədir'? '1C: Mühasibat 8 mühasib' üçün peşəkar bir vasitədir, onun köməyi ilə mühasibat və vergi uçotunu apara, məcburi hesabatlar hazırlayıb təqdim edə bilərsiniz. Proqram əvvəlki versiyaların bütün nailiyyətlərini və yüz minlərlə müəssisə və təşkilatın mühasiblərinin praktiki təcrübəsinə əsaslanan yeni həlləri özündə birləşdirir.Qanuna və biznes ehtiyaclarına uyğun olaraq dəqiq mühasibat uçotu, vergilərin hesablanması, sənədlərin və biznes əməliyyatlarının işlənməsi zamanı vaxta qənaət, yüksək iş rahatlığı ilə birlikdə effektiv istifadəçi dəstəyi 1C: Mühasibat Uçotu 8-in əsas xüsusiyyətlərindən yalnız bəziləridir.",
+    link: "https://v8.1c.ru/buhv8/poleznye-materialy/tag-dlya-bukhgaltera/",
+  },
+  {
+    basliq: "1C: Müəssisənin idarəedilməsi 3.0",
+    info1:
+      "Qutulu həll mövcud qanuni tələblərə uyğundur. Məhsulun keyfiyyətinə 1C zəmanət verilir.'1C: Şirkətimizi idarə etmək' vahid məlumat sistemində saxlamağa imkan verən geniş funksional imkanlara malikdir: müştəri məlumat bazası, bank və kassa əməliyyatları, kontragentlərlə hesablaşmalar, kadrlar, büdcə, sifarişlərin, materialların, malların, məhsulların uçotu və məsrəflər, ticarət əməliyyatları, o cümlədən kommersiya avadanlığının pərakəndə satışı və qoşulması, iş sifarişlərinin, yerinə yetirilən işlərin və göstərilən xidmətlərin uçotu, əmlakın uçotu, gəlir, xərclər, mənfəət və zərər, kapitalın uçotu. Sadələşdirilmiş vergi sistemi üzrə fərdi sahibkarlar proqramda Federal Vergi Xidmətinə, FSS, PFR-ə hesabat hazırlayıb təqdim edə bilərlər.Proqram satışı, personalın iş yükünü və əsas resursları planlaşdırmağa, iş qrafiklərini, mal və materialların daşınmasını və çatdırılmasını təmin etməyə, qrafik və planların yerinə yetirilməsinə nəzarət etməyə imkan verir. Proqram bir neçə şirkət və fərdi sahibkar üçün istifadə edilə bilər - müstəqil və ya eyni biznesdə işləyən.",
+    link: "https://v8.1c.ru/small.biz/",
+  },
+  {
+    basliq: "1C:Əmək haqqı və Kadrların idarəedilməsi 3.0",
+    info1:
+      "'1C: Əmək haqqı və kadrların idarə edilməsi 8 CORP (versiya 3)' istənilən ölçülü şirkət üçün insan resurslarının effektiv idarə olunması üçün hərtərəfli sistem qurmağa imkan verir.Mürəkkəblik. Proqram bütün HR proseslərini bir sistemdə avtomatlaşdırmağa imkan verir və mühasiblər və kadrlar üzrə məsul şəxslər üçün qabaqcıl imkanlar təqdim edir. Buna görə də, 1C:ZUP CORP-da analitika uçdan-uca, vizual və çevik şəkildə konfiqurasiya edilmişdir.Mənfəətlilik. Şirkət bir çox proqram məhsullarını almaq və saxlamaq, onları bir-biri ilə inteqrasiya etmək lazım olmadığına qənaət edir. Əsas çatdırılma '1C: Əmək haqqı və kadrların idarə edilməsi 8 CORP (versiya 3)' və iş yerləri üçün lisenziyalar 1 dəfə alınır və abunə haqqı tələb olunmur. Qiymət haqqında ətraflı.Könüllülük. Hər şeyi bir anda avtomatlaşdırmaq lazım deyil, siz sistemi biznes proseslərinizə və prioritetlərinizə uyğun olaraq tədricən konfiqurasiya edə bilərsiniz.İstifadə təhlükəsizliyi. Giriş hüquqlarının parametrləri sistemi proses iştirakçılarına yalnız onların vəzifə öhdəliklərinin səviyyəsinə uyğun olan məlumatları təqdim etməyə imkan verir. Bu, məlumat və şəxsi məlumatlarla işləməyin təhlükəsizliyini təmin edir.Qutulu həll mövcud qanuni tələblərə uyğundur. Məhsulun keyfiyyətinə 1C zəmanət verilir.",
+    link: "https://v8.1c.ru/hrmcorp/",
+  },
+  {
+    basliq: "1C: Kompleks avtomatlaşdırma 2.5",
+    info1:
+      "'1C: İnteqrasiya edilmiş avtomatlaşdırma' müəssisədə idarəetmə və mühasibat uçotunun əsas vəzifələrini əhatə edən vahid məlumat sisteminin qurulması proqramıdır. Bu həll ən vacib biznes sahələrini avtomatlaşdırmağa imkan verir: mühasibat uçotu, ticarət, anbar, əmək haqqı, kadr uçotu.Tətbiq olunan həll alətlərindən istifadə hər iki şöbənin təşkilat daxilində və xarici mühitlə (müştərilər, təchizatçılar, rəqiblər) əlaqələndirilmiş işini təmin etməyə imkan verir.'1C: İnteqrasiya edilmiş avtomatlaşdırma' proqramının məqsədiProqram kiçik və orta biznes sahəsində: ticarət müəssisələrində, kiçik sənaye sahələrində, xidmət göstərən şirkətlərdə istifadəyə yönəlib. '1C: İnteqrasiya edilmiş avtomatlaşdırma' hazırda bir neçə ayrı məhsuldan istifadə edən, eləcə də idarəetmə və mühasibat ehtiyacları mövcud informasiya sistemlərinin imkanlarını aşmış şirkətlər üçün ən uyğundur.Biznes tələbləri və qanunvericilik dəyişdikcə, bu məhsul funksionallıqdan daha yaxşı istifadə, 1C:Enterprise 8 platformasındakı digər proqramlarla və digər həllər ilə inteqrasiya yolu ilə yenidən konfiqurasiya edilə, genişləndirilə bilər.İcra ssenariləri '1C: İnteqrasiya edilmiş avtomatlaşdırm'Bu proqram məhsulu təşkilata mövcud şəraitdə vəzifə və planlara uyğun olaraq onun həyata keçirilməsi üçün ən əlverişli variantı seçməyə imkan verir.Çox məhsulu müasir, inteqrasiya olunmuş həll ilə əvəz etmək. '1C: İnteqrasiya edilmiş avtomatlaşdırma' əvvəlki versiyaların istifadə olunan kompleks həllərini əvəz etmək üçün lazımdır. Yeni nəslin müasir proqram məhsulu iş proseslərini ən effektiv şəkildə avtomatlaşdırmağa və onun həyata keçirilməsi üçün optimal siyasət həyata keçirməyə imkan verəcək.Müəssisə informasiya sisteminin addım-addım inkişafı. Proqram müəssisənin informasiya sisteminin mərhələli inkişafını təmin edir - fərdi proqramlar və ya onlarla əlaqəli komplekslər əsasında prioritet vəzifələrin həllindən tutmuş '1C: İnteqrasiya edilmiş Avtomatlaşdırma 2' əsasında vahid müəssisə məlumat sisteminin yaradılmasına qədər.Fərdi tapşırıqların avtomatlaşdırılması. Məhsula daxil olan 1C: Enterprise 8 proqramlarından istifadə edərək, ən vacib iş tapşırıqlarını avtomatlaşdıra və ya müəssisə üçün əlverişli olan digər variantlardan istifadə edə bilərsiniz.",
+    link: "https://v8.1c.ru/ka/",
+  },
+  {
+    basliq: "1C: ERP 2.5",
+    info1:
+      "1C:ERP Enterprise Management iri və orta biznesin avtomatlaşdırılması üzrə ən yaxşı qlobal və yerli təcrübələri nəzərə alaraq şaxələndirilmiş müəssisələrin fəaliyyətini idarə etmək üçün mürəkkəb informasiya sistemlərinin qurulması üçün innovativ həlldir. 1C: ERP Enterprise Management həlli aparıcı 1C tərəfdaşlarının (ERP Mərkəzləri, İnkişaf) mütəxəssislərinin daxil olduğu xüsusi yaradılmış ekspert şurasının iştirakı ilə 1C mütəxəssislərindən ibarət layihə qrupu tərəfindən 1C: Müəssisə platformasının yeni müasir versiyası 8.3-də hazırlanmışdır. Mərkəzlər ' 1C') və böyük sənaye müəssisələrinin ixtisaslaşmış şöbələrinin rəhbərləri. Son versiyanın buraxılmasından əvvəl yüzlərlə tərəfdaş və onlarla müştəri bir ildən artıqdır ki, bu məhsulu pilot tətbiqlərdə öyrənir və sınaqdan keçirir.İnkişaf zamanı iri müəssisələrin müxtəlif fəaliyyət sahələrində, o cümlədən texniki cəhətdən mürəkkəb çox emal istehsalı olan müəssisələrdə tələb olunan funksionallığın həyata keçirilməsinə xüsusi diqqət yetirilmişdir.",
+    link: "https://solutions.1c.ru/catalog/1cerp/features",
+  },
+  {
+    basliq: "1C: Holdingin idarəedilməsi",
+    info1:
+      "'1C: Holding Management 8' idarəetmə şirkətlərinin işinin və holdinqlərin biznes bölmələrinin maliyyə xidmətlərinin səmərəli avtomatlaşdırılması üçün bir vasitədir. Adekvat icra müddəti, aşağı dəstəyin dəyəri ilə birlikdə, prinsipcə yeni idarəetmə və maliyyə nəticələrini tez bir zamanda əldə etməyə imkan verir. '1C: Holding Management 8' proqramının dəqiq qiyməti və həyata keçirilmə vaxtı şirkətinizin layihədən əvvəlki sorğusu zamanı 1C tərəfdaşlarının mütəxəssisləri tərəfindən müəyyən edilir.Məhsuldan mühasibat uçotu, büdcə tərtibatı, biznesin təhlili və mərkəzləşdirilmiş funksiyaların avtomatlaşdırılması üçün istifadə edən ölkə üzrə 600-dən çox korporativ müştəri artıq “1C: Holding Management 8” tətbiqinin effektivliyini yoxlaya bilib. CPM sinifinin kompleks həlli 1C platformasına əsaslanan istənilən həllər ilə effektiv şəkildə birləşdirilə bilər.Əməliyyat və mühasibat uçotunun avtomatlaşdırılması, MHBS hesabatlarının hazırlanması, korporativ maliyyə və satınalmaların idarə edilməsi üçün bir məlumat bazasına üstünlük verən şirkətlər üçün “1C: ERP. '1C: Holdinq İdarəetmə' və '1C: ERP' funksiyalarını birləşdirən Holdinq İdarəetmə. Müəssisə İdarəetmə' bir sistemdə.",
+    link: "https://v8.1c.ru/cpm/",
+  },
+  {
+    basliq: "1C: Sənəd dövriyyəsi",
+    info1:
+      "Elə bir vaxt gəlir ki, siz müəssisənizdə sənədlərlə işi avtomatlaşdırmağa qərar verirsiniz. Bu, müxtəlif səbəblərdən baş verir: məsələn, koordinasiya və qərarların qəbulu üçün vaxtın azaldılması, sənədlərin itirilməməsi və hər zaman əlinizdə olması, icra intizamını yoxlamaq. Bunu etmək üçün yaxşı bir vasitə və onu istifadə etmək üçün sübut edilmiş bir texnika lazımdır.Bu, '1C: Sənəd İdarəetmə 8'dir:biznes proseslərinin idarə edilməsi və işçilərin əməkdaşlığı üçün geniş seçimlərə malik müasir ECM-sistemi (Müəssisə Məzmununun İdarəedilməsi);elektron sənəd dövriyyəsini təşkil etməyə, prosesləri qurmağa, tapşırıqların icrasına nəzarəti təmin etməyə, idarəetmə fəaliyyətini tənzimləməyə və onun səmərəliliyini artırmağa kömək edəcək sübut edilmiş metod və təcrübələr.",
+    link: "https://v8.1c.ru/doc8/",
+  },
+];
+
+for (let i = 0; i < menu.length; i++) {
+  document.querySelector(
+    ".productions-wrapper ul"
+  ).innerHTML += `<li onclick="funk(${i})">${menu[i].basliq}</li>`;
+}
+
+function funk(i) {
+  document.querySelector(".productions-status p").innerText = menu[i].info1;
+  document.querySelector(".productions-icon a").href = menu[i].link;
+  document.querySelector(".productions-icon img").href = menu[i].info5;
+}
+
+const listItems = document.querySelectorAll(".productions-wrapper ul li");
+listItems[0].classList.add("active");
+listItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    listItems.forEach((item) => {
+      item.classList.remove("active");
+    });
+    item.classList.add("active");
+  });
+});
