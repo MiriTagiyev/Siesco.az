aa0009dc5c760d07629f25ba0dbaa0ba89029ab5
#MT <miri.tagiyev999@gmail.com>
2023-05-22 15:54:26 +0400

    sssssssss

--- services.js
+++ services.js
@@ -15,53 +15,76 @@ window.onload = () => {
   $("#it-services>ul>li:first-child").click();
 };
 
-var situation = [
-  {
-    basliq: "1C:Françayzinq",
-    sual: "1C:Françayzinq nədir?",
-    photo: "/img/Mikro.png",
-    info1:
-      "Əməkdaşlarımız - «1С» tərəfindən sertifikatlaşdırılmış, «1С» proqramının sadə quraşdırılması və konfiqurasiyasından başlayaraq proqramın digər paketlərlə inteqrasiyasına, istifadəçi təlimlərinə, mühasibat uçotunun və ofis işlərinin avtomatlaşdırılmasına qədər hərtərəfli xidmətləri yerinə yetirmək qabiliyyətinə malik mütəxəssislərdir.",
-  },
-  {
-    basliq: "1C:Real Avtomatlaşdırma Mərkəzi",
-    sual: "1C:Real Avtomatlaşdırma Mərkəzi nədir?",
-    photo: "/img/1CNew.png",
-    info1:
-      "Real Avtomatlaşdırma Mərkəzlərinin əməkdaşları tələblərin səmərəli müəyyən edilməsi texnologiyası üzrə təlim keçmiş və sertifikatlaşdırılmışdırlar – onlar «1С» standartlarına uyğun olaraq şirkətin biznes proseslərinin ilkin qiymətləndirilməsini tez və keyfiyyətli şəkildə aparmağa kömək edəcək və müştərinin biznes effektivliyini artırma metodlarını təklif edəcəklər",
-  },
-  {
-    basliq: "1C:ERP Mərkəzi - namizəd",
-    sual: "1C:ERP Mərkəzi nədir?",
-    photo: "/img/it.png",
-    info1:
-      "'1C: ERP Mərkəzi' statusuna malik tərəfdaş 1C ERP həllərinin müntəzəm satışı,tətbiqlərin həyata keçirilməsi, habelə digər tərəfdaşlar tərəfindən həyata keçirilən layihələrə nəzarət etmək hüququna malikdir.Status müəssisənin idarə edilməsi üçün «1С» şirkətinin ERP-həllərində səlahiyyətlərin olmasını ifadə edir; bütün alt sistemlər üzrə mütəxəssislər: Konsultantlar, sertifikatlı layihə menecerləri; uğurlu tətbiqlər; keyfiyyət idarəetmə sistemləri.",
-  },
-  {
-    basliq: "1C-Bitriks tərəfdaşı",
-    sual: "1C-Bitriks nədir?",
-    photo: "/img/bitrix.png",
-    info1: "1C-Bitriks tərəfdaşı",
-  },
-];
+// var situation = [
+//   {
+//     basliq: "1C:Françayzinq",
+//     sual: "1C:Françayzinq nədir?",
+//     photo: "/img/Mikro.png",
+//     info1:
+//       "Əməkdaşlarımız - «1С» tərəfindən sertifikatlaşdırılmış, «1С» proqramının sadə quraşdırılması və konfiqurasiyasından başlayaraq proqramın digər paketlərlə inteqrasiyasına, istifadəçi təlimlərinə, mühasibat uçotunun və ofis işlərinin avtomatlaşdırılmasına qədər hərtərəfli xidmətləri yerinə yetirmək qabiliyyətinə malik mütəxəssislərdir.",
+//   },
+//   {
+//     basliq: "1C:Real Avtomatlaşdırma Mərkəzi",
+//     sual: "1C:Real Avtomatlaşdırma Mərkəzi nədir?",
+//     photo: "/img/1CNew.png",
+//     info1:
+//       "Real Avtomatlaşdırma Mərkəzlərinin əməkdaşları tələblərin səmərəli müəyyən edilməsi texnologiyası üzrə təlim keçmiş və sertifikatlaşdırılmışdırlar – onlar «1С» standartlarına uyğun olaraq şirkətin biznes proseslərinin ilkin qiymətləndirilməsini tez və keyfiyyətli şəkildə aparmağa kömək edəcək və müştərinin biznes effektivliyini artırma metodlarını təklif edəcəklər",
+//   },
+//   {
+//     basliq: "1C:ERP Mərkəzi - namizəd",
+//     sual: "1C:ERP Mərkəzi nədir?",
+//     photo: "/img/it.png",
+//     info1:
+//       "'1C: ERP Mərkəzi' statusuna malik tərəfdaş 1C ERP həllərinin müntəzəm satışı,tətbiqlərin həyata keçirilməsi, habelə digər tərəfdaşlar tərəfindən həyata keçirilən layihələrə nəzarət etmək hüququna malikdir.Status müəssisənin idarə edilməsi üçün «1С» şirkətinin ERP-həllərində səlahiyyətlərin olmasını ifadə edir; bütün alt sistemlər üzrə mütəxəssislər: Konsultantlar, sertifikatlı layihə menecerləri; uğurlu tətbiqlər; keyfiyyət idarəetmə sistemləri.",
+//   },
+//   {
+//     basliq: "1C-Bitriks tərəfdaşı",
+//     sual: "1C-Bitriks nədir?",
+//     photo: "/img/bitrix.png",
+//     info1: "1C-Bitriks tərəfdaşı",
+//   },
+// ];
 
-for (let i = 0; i < situation.length; i++) {
-  document.querySelector(
-    "#status-services ul"
-  ).innerHTML += `<li onclick="funk(${i})">${situation[i].basliq}</li>`;
-}
+// for (let i = 0; i < situation.length; i++) {
+//   document.querySelector(
+//     "#status-services ul"
+//   ).innerHTML += `<li onclick="funk(${i})">${situation[i].basliq}</li>`;
+// }
 
-function funk(i) {
-  document.querySelector(".status-info").innerText = situation[i].info1;
-  document.querySelector(".status-photo img").src = situation[i].photo;
-  // document.querySelector(".status-basliq").innerText = situation[i].sual;
-}
+// function funk(i) {
+//   document.querySelector(".status-info").innerText = situation[i].info1;
+//   document.querySelector(".status-photo img").src = situation[i].photo;
+// }
 
-let lastStatus = "web";
-$(document).ready(function () {
-  $("#status-services>ul>li:first").addClass("active");
-  $("#status-services>ul>li").click(function () {
-    $("#status-services>ul>li.active").removeClass("active");
-    $(this).addClass("active");
-  });
+// let lastStatus = "web";
+// $(document).ready(function () {
+//   $("#status-services>ul>li:first").addClass("active");
+//   $("#status-services>ul>li").click(function () {
+//     $("#status-services>ul>li.active").removeClass("active");
+//     $(this).addClass("active");
+//   });
+// });
+
+$("#chatme").click(function () {
+  $(".widget").toggleClass("open");
+});
+
+const currentYear = new Date().getFullYear();
+const footerYear = document.getElementById("footer-year");
+footerYear.textContent = `© ${currentYear} «Siesco» - Bütün hüquqlar qorunur.`;
+
+$("#mobile").click(function () {
+  $(".webtemp").hide();
+});
+$("#idare").click(function () {
+  $(".webtemp").hide();
+});
+$("#smm").click(function () {
+  $(".webtemp").hide();
+});
+$("#design").click(function () {
+  $(".webtemp").hide();
+});
+$("#web").click(function () {
+  $(".webtemp").show();
 });
