95f96ea19218eaf3829f37dd37c7110afc5a705f
#MT <miri.tagiyev999@gmail.com>
2023-06-03 18:29:03 +0400

    ssssssss

--- bitrix.js
+++ bitrix.js
@@ -1,31 +1,35 @@
 let lastService = "web";
-$("#readmore-services>ul>li").click(function () {
-  $("#readmore-services>ul>li.active").removeClass("active");
+$("#bitrixreadmore-services>ul>li").click(function () {
+  $("#bitrixreadmore-services>ul>li.active").removeClass("active");
   $(this).addClass("active");
   const currentService = $(this).attr("href");
-  $(".readmore-detail ." + lastService + "-text").animate(
+  $(".bitriksreadmore-detail ." + lastService + "-text").animate(
     { left: "-100%" },
     () => {
-      $(".readmore-detail ." + currentService + "-text").animate({ left: "0" });
+      $(".bitriksreadmore-detail ." + currentService + "-text").animate({
+        left: "0",
+      });
     }
   );
-  $(".readmore-detail ." + lastService + "-img").animate(
+  $(".bitriksreadmore-detail ." + lastService + "-img").animate(
     { right: "-100%" },
     () => {
-      $(".readmore-detail ." + currentService + "-img").animate({ right: "0" });
+      $(".bitriksreadmore-detail ." + currentService + "-img").animate({
+        right: "0",
+      });
     }
   );
   lastService = currentService;
 });
 window.onload = () => {
-  $("#readmore-services>ul>li:first-child").click();
+  $("#bitrixreadmore-services>ul>li:first-child").click();
 };
 
 let lastStatus = "web";
 $(document).ready(function () {
-  $("#readmore-services>ul>li:first").addClass("active");
-  $("#readmore-services>ul>li").click(function () {
-    $("#readmore-services>ul>li.active").removeClass("active");
+  $("#bitrixreadmore-services>ul>li:first").addClass("active");
+  $("#bitrixreadmore-services>ul>li").click(function () {
+    $("#bitrixreadmore-services>ul>li.active").removeClass("active");
     $(this).addClass("active");
   });
 });
